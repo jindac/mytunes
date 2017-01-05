a2ca6fef9ab7ea48ec804b2437502726a47a3416
jindac <dachanman91@gmail.com>
2017-01-04 16:24:45 -0800

    Implement triggers in ES6 for SongModel

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,20 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
-  play: function() {
+  play() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
-  }
+  },
 
+  enqueue() {
+    this.trigger('enqueue', this);
+  },
+
+  dequeue() {
+    this.trigger('dequeue', this);
+  },
+
+  ended() {
+    this.trigger('ended', this);
+  },
 });
